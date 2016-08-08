8bc67b4d99eb18d9fbd82994e58e12ec0337eac4
Arpit Mohan <mohanarpit@gmail.com>
2016-08-08 16:33:51 +0530

    Adding the email smtp settings so that users can submit the form to my email ID

--- php/config.php
+++ php/config.php
@@ -1,4 +1,4 @@
-<?
+<?php
 	//define the receiver of the email
 	
 	define('TO_NAME','Arpit Mohan');
@@ -8,14 +8,12 @@
 	define('TEMPLATE_PATH','template/default.php');
  
 	define('SMTP_ENABLE',true); // True to enable SMTP
-	define('SMTP_HOST','');
-	define('SMTP_USERNAME','');
-	define('SMTP_PASSWORD','');
+	define('SMTP_HOST',getenv('SMTP_HOST'));
+	define('SMTP_USERNAME',getenv('SMTP_USERNAME'));
+	define('SMTP_PASSWORD',getenv('SMTP_PASSWORD'));
 
 	// Messages
 	define('MSG_INVALID_NAME','Please enter your name.');
 	define('MSG_INVALID_EMAIL','Please enter a valid e-mail.');
 	define('MSG_INVALID_MESSAGE','Please enter your message.');
 	define('MSG_SEND_ERROR','Sorry, we can\'t send this message.');
-
-?>
